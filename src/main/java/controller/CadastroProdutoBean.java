0b11c2e5bfa5e2c9b53061d42eb131a13054b716
Adriano <adrianofalcao@gmail.com>
2019-10-31 04:10:31 -0300

    Combo boxes dependentes de categorias e subcategorias

--- src/main/java/controller/CadastroProdutoBean.java
+++ src/main/java/controller/CadastroProdutoBean.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotNull;
 import model.Categoria;
 import model.Produto;
 import repository.Categorias;
+import util.jsf.FacesUtil;
 
 @Named
 @ViewScoped
@@ -27,6 +28,7 @@ public class CadastroProdutoBean implements Serializable{
 	private Categoria categoriaPai;
 	
 	private List<Categoria> categoriaRaizes;
+	private List<Categoria> subcategorias;
 	
 	public CadastroProdutoBean() {
 		produto = new Produto();
@@ -36,14 +38,21 @@ public class CadastroProdutoBean implements Serializable{
 		//EntityManagerFactory factory = Persistence.createEntityManagerFactory("PedidoPU");
 		//EntityManager manager = factory.createEntityManager();
 		System.out.println("Inicializando...");
-		
-		categoriaRaizes =  categorias.buscarCategoriaRaizes();
+
+		if (FacesUtil.isNotPostBack()) {
+			categoriaRaizes =  categorias.buscarCategoriaRaizes();
+		}
 		
 		//manager.close();
+	}
+	
+	public void carregarSubcategorias() {
+		subcategorias = categorias.buscarSubCategoriasDe(categoriaPai);
 	}
 
 	public void salvar() {
 		System.out.println("Categoria pai selecionada: " + categoriaPai.getDescricao());
+		System.out.println("Subcategoria selecionada: " + produto.getCategoria().getDescricao());
 	}
 
 	public Produto getProduto() {
@@ -68,6 +77,10 @@ public class CadastroProdutoBean implements Serializable{
 
 	public void setCategoriaPai(Categoria categoriaPai) {
 		this.categoriaPai = categoriaPai;
+	}
+
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
 	}
 	
 }
