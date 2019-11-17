ed413a916709c8c97598ef89c991f48029c6ccb2
Adriano <adrianofalcao@gmail.com>
2019-11-17 18:51:22 -0300

    Carregamento lazy em DataTable

--- src/main/java/controller/PesquisaPedidosBean.java
+++ src/main/java/controller/PesquisaPedidosBean.java
@@ -1,12 +1,15 @@
 package controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 
 import enumeration.StatusPedido;
 import model.Pedido;
@@ -21,27 +24,38 @@ public class PesquisaPedidosBean implements Serializable {
 
 	@Inject
 	private Pedidos pedidos;
-	
+
 	private PedidoFilter filtro;
-	private List<Pedido> pedidosFiltados;
+	private LazyDataModel<Pedido> model;
 
 	public PesquisaPedidosBean() {
 		filtro = new PedidoFilter();
-		pedidosFiltados = new ArrayList<>();
-	}
-	
-	public void pesquisar() {
-		pedidosFiltados = pedidos.filtrados(filtro);
-	}
 
-	public List<Pedido> getPedidosFiltados() {
-		return pedidosFiltados;
+		model = new LazyDataModel<Pedido>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public List<Pedido> load(int first, int pageSize, String sortField, SortOrder sortOrder,
+					Map<String, Object> filters) {
+				
+				filtro.setPrimeiroRegistro(first);
+				filtro.setQuantidadeRegistros(pageSize);
+				filtro.setPropriedadeOrdenacao(sortField);
+				filtro.setAscendente(SortOrder.ASCENDING.equals(sortOrder));
+				
+				
+				setRowCount(pedidos.quantidadeFiltrados(filtro));
+				
+				return pedidos.filtrados(filtro);
+			}
+		};
 	}
 
 	public PedidoFilter getFiltro() {
 		return filtro;
 	}
-	
+
 	public StatusPedido[] getStatuses() {
 		return StatusPedido.values();
 	}
@@ -50,4 +64,7 @@ public class PesquisaPedidosBean implements Serializable {
 		this.filtro = filtro;
 	}
 
+	public LazyDataModel<Pedido> getModel() {
+		return model;
+	}
 }
--- src/main/java/repository/Pedidos.java
+++ src/main/java/repository/Pedidos.java
@@ -36,6 +36,50 @@ public class Pedidos implements Serializable {
 	@Inject
 	private EntityManager manager;
 
+	private Criteria criarCriteriaParaFiltro(PedidoFilter filtro) {
+		Session session = this.manager.unwrap(Session.class);
+		
+		Criteria criteria = session.createCriteria(Pedido.class)
+				// fazemos uma associação (join) com cliente e nomeamos como "c"
+				.createAlias("cliente", "cliente")
+				// fazemos uma associação (join) com vendedor e nomeamos como "v"
+				.createAlias("vendedor", "v");
+		
+		if (filtro.getNumeroDe() != null) {
+			// id deve ser maior ou igual (ge = greater or equals) a filtro.numeroDe
+			criteria.add(Restrictions.ge("id", filtro.getNumeroDe()));
+		}
+
+		if (filtro.getNumeroAte() != null) {
+			// id deve ser menor ou igual (le = lower or equal) a filtro.numeroDe
+			criteria.add(Restrictions.le("id", filtro.getNumeroAte()));
+		}
+
+		if (filtro.getDataCriacaoDe() != null) {
+			criteria.add(Restrictions.ge("dataCriacao", filtro.getDataCriacaoDe()));
+		}
+		
+		if (filtro.getDataCriacaoAte() != null) {
+			criteria.add(Restrictions.le("dataCriacao", filtro.getDataCriacaoAte()));
+		}
+		
+		if (StringUtils.isNotBlank(filtro.getNomeCliente())) {
+			// acessamos o nome do cliente associado ao pedido pelo alias "c", criado anteriormente
+			criteria.add(Restrictions.ilike("c.nome", filtro.getNomeCliente(), MatchMode.ANYWHERE));
+		}
+		
+		if (StringUtils.isNotBlank(filtro.getNomeVendedor())) {
+			// acessamos o nome do vendedor associado ao pedido pelo alias "v", criado anteriormente
+			criteria.add(Restrictions.ilike("v.nome", filtro.getNomeVendedor(), MatchMode.ANYWHERE));
+		}
+		
+		if (filtro.getStatuses() != null && filtro.getStatuses().length > 0) {
+			// adicionamos uma restrição "in", passando um array de constantes da enum StatusPedido
+			criteria.add(Restrictions.in("status", filtro.getStatuses()));
+		}
+		return criteria;
+	}
+	
 	@SuppressWarnings({ "unchecked" })
 	public Map<Date, BigDecimal> valoresTotaisPorData(Integer numeroDeDias, Usuario criadoPor) {
 		Session session = manager.unwrap(Session.class);
@@ -91,48 +135,19 @@ public class Pedidos implements Serializable {
 	
 	@SuppressWarnings("unchecked")
 	public List<Pedido> filtrados(PedidoFilter filtro) {
-		Session session = this.manager.unwrap(Session.class);
 		
-		Criteria criteria = session.createCriteria(Pedido.class)
-				// fazemos uma associação (join) com cliente e nomeamos como "c"
-				.createAlias("cliente", "c")
-				// fazemos uma associação (join) com vendedor e nomeamos como "v"
-				.createAlias("vendedor", "v");
+		Criteria criteria = criarCriteriaParaFiltro(filtro);
 		
-		if (filtro.getNumeroDe() != null) {
-			// id deve ser maior ou igual (ge = greater or equals) a filtro.numeroDe
-			criteria.add(Restrictions.ge("id", filtro.getNumeroDe()));
-		}
-
-		if (filtro.getNumeroAte() != null) {
-			// id deve ser menor ou igual (le = lower or equal) a filtro.numeroDe
-			criteria.add(Restrictions.le("id", filtro.getNumeroAte()));
-		}
-
-		if (filtro.getDataCriacaoDe() != null) {
-			criteria.add(Restrictions.ge("dataCriacao", filtro.getDataCriacaoDe()));
+		criteria.setFirstResult(filtro.getPrimeiroRegistro());
+		criteria.setMaxResults(filtro.getQuantidadeRegistros());
+		
+		if (filtro.isAscendente() && filtro.getPropriedadeOrdenacao() != null) {
+			criteria.addOrder(Order.asc(filtro.getPropriedadeOrdenacao()));
+		} else if (filtro.getPropriedadeOrdenacao() != null) {
+			criteria.addOrder(Order.desc(filtro.getPropriedadeOrdenacao()));
 		}
 		
-		if (filtro.getDataCriacaoAte() != null) {
-			criteria.add(Restrictions.le("dataCriacao", filtro.getDataCriacaoAte()));
-		}
-		
-		if (StringUtils.isNotBlank(filtro.getNomeCliente())) {
-			// acessamos o nome do cliente associado ao pedido pelo alias "c", criado anteriormente
-			criteria.add(Restrictions.ilike("c.nome", filtro.getNomeCliente(), MatchMode.ANYWHERE));
-		}
-		
-		if (StringUtils.isNotBlank(filtro.getNomeVendedor())) {
-			// acessamos o nome do vendedor associado ao pedido pelo alias "v", criado anteriormente
-			criteria.add(Restrictions.ilike("v.nome", filtro.getNomeVendedor(), MatchMode.ANYWHERE));
-		}
-		
-		if (filtro.getStatuses() != null && filtro.getStatuses().length > 0) {
-			// adicionamos uma restrição "in", passando um array de constantes da enum StatusPedido
-			criteria.add(Restrictions.in("status", filtro.getStatuses()));
-		}
-		
-		return criteria.addOrder(Order.asc("id")).list();
+		return criteria.list();
 	}
 
 	public Pedido guardar(Pedido pedido) {
@@ -141,6 +156,14 @@ public class Pedidos implements Serializable {
 
 	public Pedido porId(Long id) {
 		return this.manager.find(Pedido.class, id);
+	}
+
+	public int quantidadeFiltrados(PedidoFilter filtro) {
+		Criteria criteria = criarCriteriaParaFiltro(filtro);
+		
+		criteria.setProjection(Projections.rowCount());
+		
+		return ((Number) criteria.uniqueResult()).intValue();
 	}
 	
 }
